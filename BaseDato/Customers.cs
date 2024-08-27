d8b273764c59e180731c14be021475c04d550645
IvElGrimm <126293620+Ivaname99@users.noreply.github.com>
2024-08-27 01:05:11 -0600

    Código comentado

--- BaseDato/Customers.cs
+++ BaseDato/Customers.cs
@@ -6,19 +6,20 @@ using System.Threading.Tasks;
 
 namespace BaseDato
 {
-    public class Customers
+    public class Customers // El modificador de acceso public permite que la clase pueda ser accedida desde cualquier parte
     {
-        public string CustomerID { get; set; }
-        public string CompanyName { get; set; }
-        public string ContactName { get; set; }
-        public string ContactTitle { get; set; }
-        public string Address { get; set; }
-        public string City { get; set; }
-        public object Region { get; set; }
-        public string PostalCode { get; set; }
-        public string Country { get; set; }
-        public string Phone { get; set; }
-        public string Fax { get; set; }
-
+        // Definimos los parametros que nos permitirà leer y modificar los datos
+        // Los metodos {get; set;} nos permitiran obtener, leer y modificar el valor de las propiedades 
+        public string CustomerID { get; set; } // Definimos la propiedad para obtener el CustomerID
+        public string CompanyName { get; set; } // Definimos la propiedad para obtener el CompanyName
+        public string ContactName { get; set; } // Definimos la propiedad para obtener el ContactName
+        public string ContactTitle { get; set; } // Definimos la propiedad para obtener el ContactTitle
+        public string Address { get; set; } // Definimos la propiedad para obtener el Address
+        public string City { get; set; } // Definimos la propiedad para obtener el City
+        public object Region { get; set; } // Definimos la propiedad para obtener el Region
+        public string PostalCode { get; set; } // Definimos la propiedad para obtener el PostalCode
+        public string Country { get; set; } // Definimos la propiedad para obtener el Country
+        public string Phone { get; set; } // Definimos la propiedad para obtener el Phone
+        public string Fax { get; set; } // Definimos la propiedad para obtener el Fax
     }
 }
--- BaseDato/DBConectar.cs
+++ BaseDato/DBConectar.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿// Referencias del proyecto
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data.SqlClient;
@@ -10,33 +11,48 @@ namespace BaseDato
 {
     public class DBConectar
     {
-        public static string ConnectionString
+        public static string ConnectionString // Creamos una propiedad para obtener la cadena de conexión de la base de datos que queramos usar
         {
-            get
+            get // definimos loque queremos obtener, dentro del get
             {
+                // Obtiene directamente la cadena de conexión con el nombre que le asignamos en "app.config"
                 string CadenaConexion =
                     ConfigurationManager.
-                    ConnectionStrings["NWConnection"].ConnectionString;
+                    ConnectionStrings["NWConnection"]
+                    .ConnectionString; 
+                /////////////////////
 
-                SqlConnectionStringBuilder conexionBuilder = new SqlConnectionStringBuilder(CadenaConexion); //Se utiliza para cambiar los diferentes elementos respecto a la base de datos
+                // Crea un nuevo objeto usando la cadena de conexion
+                SqlConnectionStringBuilder conexionBuilder = 
+                    new SqlConnectionStringBuilder(CadenaConexion); //Se utiliza para cambiar los diferentes elementos respecto a la base de datos
+                ///////////////////// (Auto referenciación)
 
-                conexionBuilder.ApplicationName = ApplicationName ?? conexionBuilder.ApplicationName;
+                // Definimos el nombre de la aplicación (si no es nulo)
+                conexionBuilder.ApplicationName = 
+                    ApplicationName ?? conexionBuilder.ApplicationName;
+                /////////////////////
 
+                // Definimos el tiempo de espera de la conexion (si es mayor a 0, sino que use el valor por defecto)
                 conexionBuilder.ConnectTimeout =
                     (ConnectionTimeout > 0) ? ConnectionTimeout : conexionBuilder.ConnectTimeout;
-
+                /////////////////////
+                
+                // Retornamos la nueva conexión como una cadena de texto
                 return conexionBuilder.ToString();
+                /////////////////////
             }
         }
-        public static string ApplicationName { get; set; }
-        public static int ConnectionTimeout { get; set; }
+        public static string ApplicationName { get; set; } // Definimos la propiedad para el Nombre de la aplicación
+        public static int ConnectionTimeout { get; set; } // Definimos la propiedad para el tiempo de conexión
 
+        // Definimos un metodo para obtener una conexion SQL
         public static SqlConnection GetSql()
         {
-            SqlConnection conexion = new SqlConnection(ConnectionString);
-            conexion.Open();
-            conexion.Close();
+            SqlConnection conexion = new SqlConnection(ConnectionString); // crea una nueva conexión en base a la que obtuvimos antes
+            conexion.Open(); // Abre la conexión a la base de datos
+            return conexion; // Retorna la conexión abierta
         }
+        /////////////////////
     }
 }
 
